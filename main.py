2ef36e15b9696e80e2da994c2b909aa607490edc
Niicck <niicck@users.noreply.github.com>
2020-01-02 13:07:55 -0600

    finish fleshing out clean up logic

--- main.py
+++ main.py
@@ -22,6 +22,26 @@ app.register_error_handler(500, handle_internal_server_error)
 # Get connection to heroku api
 heroku_conn = heroku3.from_key(os.getenv('HEROKU_KEY'))
 
+# Deletes a Heroku PR build only if its
+# not master
+# not production
+# and without DELETION_PROTECTION
+def clean_up_app(branch):
+    if ((branch != "master") and (branch != "production")):
+        app_name = re.sub(r'-*$','', f'joplin-pr-{branch}'[0:30]).lower()
+        try:
+            heroku_app = heroku_conn.apps()[app_name]
+        except KeyError:
+            print(f"App {app_name} has not been built yet")
+            return
+        config = heroku_app.config()
+        if not config["DELETION_PROTECTION"]:
+            print(f"Starting to deleting app {app_name}")
+            heroku_app.delete()
+            print(f"Successfully deleted app {app_name}")
+        else:
+            print(f"DELETION_PROTECTION enabled for {app_name}, skipping clean-up.")
+
 @app.route("/")
 def hello_world():
     print('Hello World!')
@@ -31,14 +51,15 @@ def hello_world():
 @webhook.hook(event_type='pull_request')
 def on_pull_request(data):
     print(json.dumps(data))
-    # TODO: is it actually called "merged"?
-    # TODO: get a HEROKU_KEY
-    if (data["action"] == "closed") or (data["action"] == "merged"):
-        app_name = re.sub(r'-*$','', f'joplin-pr-{data["pull_request"]["head"]["ref"]}'[0:30])
-        heroku_app = heroku_conn.apps()[app_name]
-        print(f"Starting to deleting app {app_name}")
-        heroku_app.delete()
-        print(f"Successfully deleted app {app_name}")
+    action = data["action"]
+    branch = data["pull_request"]["head"]["ref"]
+
+    if (action == "closed"):
+        clean_up_app(branch)
+
+def cron_clean_up():
+    print('Clean up old branches every week.')
+    return "Good bye!"
 
 # Only needed for local development
 # Zappa handles the "app" object directly
